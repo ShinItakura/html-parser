7620301a5555a9cb8904dc6642015c9006b29f32
ShinItakura <shintaroitakura@gmail.com>
2025-11-19 13:04:53 -0800

    removed unnecessary code

--- scrypt.py
+++ scrypt.py
@@ -1,29 +1,5 @@
 from bs4 import BeautifulSoup
 
-# def find_nested_pattern(html: str):
-#     soup = BeautifulSoup(html, "html.parser")
-#     results = []
-
-#     for section in soup.find_all("section", attrs={"data-id": True}):
-#         if not str(section["data-id"]).startswith("92"):
-#             continue
-        
-#         for article in section.find_all("article", attrs={"data-class": True}):
-#             if not str(section["data-class"]).endswith("45"):
-#                 continue
-            
-#             for div in article.find_all("div", attrs={"data-tag": True}):
-#                 if "78" in str(div["data-tag"]):
-#                     results.append(div)
-    
-#     return results
-
-# def find_special_divs(html: str):
-#     soup = BeautifulSoup(html, "html.parser")
-
-#     selector = 'section[data-id^="92"] article[data-class$="45"] div[data-tag*="78"]'
-#     return soup.select(selector)
-
 def find_ramp_ref_value(html: str):
     soup = BeautifulSoup(html, "html.parser")
 
@@ -50,10 +26,6 @@ if __name__ == "__main__":
     with open("input.html", "r", encoding="utf-8") as f:
         html = f.read()
     
-    # matches = find_nested_pattern(html)
-
-    # matches = find_special_divs(html)
-
     matches = find_ramp_ref_value(html)
 
     urlmatch = ""
